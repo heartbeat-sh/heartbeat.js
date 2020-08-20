1528a8c212a76d0aef70b3f96fbbc763ba09bf4f
Arie De Bok <arie@directdebit.co.za>
2020-08-20 14:41:46 +0200

    Implement initial lib

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("init")
+import axios from 'axios'
+
+
+export default {
+    HeartbeatClient: class {
+        constructor({subdomain}) {
+            if(!subdomain) {
+                throw "Please provide a subdomain in your config!"
+            }
+
+            this.config = {
+                subdomain: subdomain
+            }
+
+            this.proto = 'https'
+            this.host = 'heartbeat.sh'
+        }
+
+        SendHeartbeat(name, warning, error) {
+            let query = ''
+            if(warning && typeof warning == "number") {
+                query = '?warning=' + parseInt(warning)
+            }
+            if(error && typeof error == "number") {
+                query += query ? '&' : '?'
+                query += 'error=' + parseInt(error)
+            }
+            let url = `${this.proto}'://'${this.host}/${name}${query}`
+
+            return axios({
+                method: 'post',
+                url: url
+            })
+        }
+    }
+}
